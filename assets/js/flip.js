254d4d514b575ca87d1f54cb6c610707e4022341
John Doyle <66917690+Aramantos@users.noreply.github.com>
2022-09-08 18:23:15 +0100

    update jss and css files to imporve animations

--- assets/js/flip.js
+++ assets/js/flip.js
@@ -13,12 +13,14 @@
 $(document).ready(function () {
   $(".flip-card-inner").click(function () {
     $("#overlay").css("display", "block");
+    $(".container").removeClass("unblur");
     $(".container").addClass("blur");
-    $(".card").css("display", "block");;
-    // $(".card").addClass("appear");
+    $(".card").css("display", "block");
+    $(".card").addClass("appear");
   });
   $("#reset-btn").click(function () {
     $("#overlay").css("display", "none");
+    $(".container").removeClass("blur");
     $(".container").addClass("unblur");
     $(".card").hide();
   });
